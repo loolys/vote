4c6a925d89b1b1909e4191329909057156e9dd7f
loolys <pierre.liljekvist@outlook.com>
2017-02-08 08:46:08 +0100

    Added some links for routing

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
 
 import App from './components/App/App.js';
+import IndexPage from './components/IndexPage/IndexPage.js';
 import About from './components/About/About.js'
+import SignUp from './components/SignUp/SignUp.js'
 
 const Routes = (props) => (
   <Router {...props}>
-    <Route path="/" component={App} />
-    <Route path="/about" component={About} />
+    <Route path="/" component={App}>
+      <IndexRoute component={IndexPage} />
+      <Route path="/about" component={About} />
+      <Route path="/signup" component={SignUp} />
+    </Route>
   </Router>
 );
 
